3624dc636508305eb973b3568f7c4059a4a756ee
Rexios <rexios@rexios.dev>
2022-09-11 00:44:56 -0400

    The socket works

--- Websocket Test WatchKit Extension/Websocket_TestApp.swift
+++ Websocket Test WatchKit Extension/Websocket_TestApp.swift
@@ -5,6 +5,8 @@
 //  Created by Rexios on 9/10/22.
 //
 
+import Network
+import NWWebSocket
 import SwiftUI
 
 @main
@@ -18,4 +20,34 @@ struct Websocket_TestApp: App {
     }
 }
 
-class SocketClient {}
+class SocketClient: WebSocketConnectionDelegate {
+    init() {
+        let socket = NWWebSocket(url: URL(string: "ws://localhost:3476")!)
+        socket.delegate = self
+        socket.connect()
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            socket.send(string: "Echo!")
+        }
+    }
+
+    func webSocketDidConnect(connection: WebSocketConnection) {
+        print("Connected")
+    }
+    
+    func webSocketDidDisconnect(connection: WebSocketConnection, closeCode: NWProtocolWebSocket.CloseCode, reason: Data?) {}
+    
+    func webSocketViabilityDidChange(connection: WebSocketConnection, isViable: Bool) {}
+    
+    func webSocketDidAttemptBetterPathMigration(result: Result<WebSocketConnection, NWError>) {}
+    
+    func webSocketDidReceiveError(connection: WebSocketConnection, error: NWError) {}
+    
+    func webSocketDidReceivePong(connection: WebSocketConnection) {}
+    
+    func webSocketDidReceiveMessage(connection: WebSocketConnection, string: String) {
+        print(string)
+    }
+    
+    func webSocketDidReceiveMessage(connection: WebSocketConnection, data: Data) {}
+}
