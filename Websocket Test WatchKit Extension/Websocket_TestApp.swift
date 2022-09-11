7024942fa88393322523e19d466c25d06f6055f4
Rexios <rexios@rexios.dev>
2022-09-10 23:59:14 -0400

    Initial commit

---
+++ Websocket Test WatchKit Extension/Websocket_TestApp.swift
@@ -0,0 +1,20 @@
+//
+//  Websocket_TestApp.swift
+//  Websocket Test WatchKit Extension
+//
+//  Created by Rexios on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Websocket_TestApp: App {
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            Text("Websocket Running")
+        }
+    }
+}
+
